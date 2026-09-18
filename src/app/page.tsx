42b2b3850db70ef64647a67bd940df5ea8aba050
CustomRomHarsh <customromharsh@gmail.com>
2026-09-18 22:23:36 +0530

    Add src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">src/app/page.tsx</code>
+        </p>
+      </div>
+      <div className="text-center">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Next.js App Deployed Successfully!
+        </h1>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          Automated deployment via GitHub and Vercel APIs.
+        </p>
+      </div>
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left"></div>
+    </main>
+  );
+}
