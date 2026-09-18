9258af04714e9b1c077349d9e8782d403a5271b0
CustomRomHarsh <customromharsh@gmail.com>
2026-09-18 22:23:36 +0530

    Add src/app/layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+
+import "./globals.css";
+export const metadata = {
+  title: "Next.js App",
+  description: "Created for automated deployment",
+};
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
